ecd799b939bac797017dfce3fa5b5c577bc5ac5d
Eremey Remzin <hungry.green.bee@gmail.com>
2022-05-09 12:12:30 +0300

    logger was added

--- ml_project/train.py
+++ ml_project/train.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 
 import pandas
+import logging
 
 
 def train_model(features: pandas.DataFrame, target: pandas.DataFrame, argues):
@@ -13,21 +14,33 @@ def train_model(features: pandas.DataFrame, target: pandas.DataFrame, argues):
             n_estimators=argues.n_estimators, random_state=argues.random_state,
             n_jobs=argues.n_jobs
         )
+        logging.info("RandomForestRegressor was initialized")
     elif argues.model_type == "LinearRegression":
         model = LinearRegression()
+        logging.info("RandomForestRegressor was initialized")
     else:
         raise NotImplementedError()
+    logging.info("Fitting was started")
+
     model.fit(features, target)
+    logging.info("Fitting was ended")
     return model
 
 
 def train(argues):
     x_train, _, y_train, _ = get_dataset(argues)
+    logging.info("Dataset were gotten")
+
     model = train_model(x_train, y_train, argues)
+    logging.info("Model was obtained")
+
     save_model(model, argues)
+    logging.info("Model was saved")
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
     args = args_parser()
     train(args)
 
